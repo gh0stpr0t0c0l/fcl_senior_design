85e0d464e672f718c86f1a289f370d4cc29944e7
GabeS <gabe@southwell.net>
2025-12-30 14:41:04 -0500

    Basic modular wifi implementation
    Will add more features and refinement later

--- drone-firmware/main/main.c
+++ drone-firmware/main/main.c
@@ -19,17 +19,12 @@
 #include "esp_err.h"
 #include "esp_log.h"
 
-#include "lwip/sockets.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "nvs_flash.h"
-#include "esp_netif.h"
-
 #include <arpa/inet.h>
 
 #include "board.h"
 #include "storage.h"
 #include "telemetry.h"
+#include "wifi.h"
 
 // i2c declarations
 static I2cDrv i2c_bus_instance;
@@ -42,65 +37,7 @@ static const I2cDef I2cConfig= {
     .gpioPullup = GPIO_PULLUP_ENABLE
 };
 
-//wifi defs
-#define WIFI_SSID "DRONE_WIFI"
-#define WIFI_PASS "password"
-#define UDP_PORT 1234
-//#define BROADCAST_IP "192.168.4.255"
-#define TELEMETRY_MAX_LEN 128
-#define TELEMETRY_QUEUE_LEN 16
-//#define MULTICAST_IP "239.1.1.1"
-#define UNICAST_IP "192.168.4.2"
-
-//Telemetry defs
-// #include "freertos/semphr.h"
-// static char ram_buffer[BUFFER_SIZE];
-// static size_t buffer_index = 0;
-// static QueueHandle_t telemetry_queue = NULL;
-
-// typedef struct
-// {
-//     uint16_t len;
-//     char buf[TELEMETRY_MAX_LEN];
-// } telemetry_msg_t;
-
-// typedef struct
-// {
-//     //MPU data
-//     int64_t mpu_timestamp_us;
-//     float pitch;
-//     float roll;
-//     //vl53l1 data
-//     int64_t tof_timestamp_us[TOF_COUNT];
-//     int16_t ranges[TOF_COUNT];
-// } telemetry_snapshot_t;
-
-// static telemetry_snapshot_t latest_snapshot;
-// static SemaphoreHandle_t snapshot_mutex = NULL;
-// static TaskHandle_t telemetry_aggregator_handle = NULL;
-
 static const char *TAG = "Drone";
-
-//init the udp broadcast
-void wifi_init(void) {
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-    esp_wifi_set_mode(WIFI_MODE_AP);
-    wifi_config_t ap_config = {
-        .ap = {
-            .ssid = WIFI_SSID,
-            .ssid_len = strlen(WIFI_SSID),
-            .password = WIFI_PASS,
-            .max_connection = 4,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK,
-        },
-    };
-    if (strlen(WIFI_PASS) == 0) {
-        ap_config.ap.authmode = WIFI_AUTH_OPEN;
-    }
-    esp_wifi_set_config(WIFI_IF_AP, &ap_config);
-    esp_wifi_start();
-}
 
 void tof_logging(void *pvPerameter)
 {
@@ -275,178 +212,13 @@ void blinky(void *pvParameter)
 // I tried multicast and I couldnt get it to work
 // Works for now
 // TODO: Make this use multicast so multiple computers can connect
-void telemetry_broadcast(void *pvParameter)
-{
-    int sock = -1;
-    struct sockaddr_in serv_addr, unicast_addr;
-
-    vTaskDelay(pdMS_TO_TICKS(500));
-
-    //create socket
-    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sock < 0) {
-        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-        vTaskDelete(NULL);
-        return;
-    }
-
-    //config socket
-    uint8_t ttl = 1;
-    if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl)) < 0) {
-        ESP_LOGE(TAG, "Failed to set multicast TTL");
-        close(sock);
-        vTaskDelete(NULL);
-        return;
-    }
-
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(0);
-
-    //bind socket
-    if (bind(sock, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        ESP_LOGE(TAG, "failed to bind: errno %d", errno);
-        close(sock);
-        vTaskDelete(NULL);
-        return;
-    }
-
-    memset(&unicast_addr, 0, sizeof(unicast_addr));
-    unicast_addr.sin_family = AF_INET;
-    unicast_addr.sin_port = htons(UDP_PORT);
-    inet_aton(UNICAST_IP, &unicast_addr.sin_addr);
-
-    ESP_LOGI(TAG, "UDP broadcast task started, sending to %s:%d", UNICAST_IP, UDP_PORT);
-
-    telemetry_msg_t msg;
-
-    QueueHandle_t telemetry_queue = telemetry_get_queue();
-
-    //send loop
-    while(1){
-        //block task until message in queue
-        if (xQueueReceive(telemetry_queue, &msg, portMAX_DELAY) == pdTRUE){
-            int sent = sendto(sock, msg.buf, msg.len, 0 , (struct sockaddr *)&unicast_addr, sizeof(unicast_addr));
-            //ESP_LOGE(TAG,"%d %s", strlen(msg.buf), msg.buf);
-            if (sent < 0){
-                ESP_LOGE(TAG, "Error occurred during sendto: errno %d (%s)", errno, strerror(errno));
-            }
-        }
-    }
-}
-
-// void telemetry_aggregator(void *pvPerameter)
-// {
-//     const TickType_t agg_period = pdMS_TO_TICKS(50);
-
-//     telemetry_snapshot_t local;
-//     telemetry_snapshot_t last_snap = {0};
-
-//     const int64_t stale_mpu_time = 150000LL; // Will need adjust
-//     const int64_t stale_tof_time = 150000LL; // Will need to adjust
-
-
-//     // double mpu_time_s;
-//     // double tof_time_s[TOF_COUNT];
-//     bool mpu_stale;
-//     bool tof_stale[TOF_COUNT];
-
-//     char csv_line[512];
-
-//     while(1){
-//         vTaskDelay(agg_period);
-
-//         // Take mutex and copy data
-//         if(xSemaphoreTake(snapshot_mutex, pdMS_TO_TICKS(10)) == pdTRUE){
-//             local = latest_snapshot;
-//             xSemaphoreGive(snapshot_mutex);
-//         }
-//         else {
-//             continue;
-//         }
-
-//         // Skip if no new data
-//         bool any_changed = false;
-//         if (local.mpu_timestamp_us != last_snap.mpu_timestamp_us) {
-//             any_changed = true;
-//         } else {
-//             for (uint8_t i = 0; i < TOF_COUNT; ++i) {
-//                 if (local.tof_timestamp_us[i] != last_snap.tof_timestamp_us[i]) {
-//                     any_changed = true;
-//                     break;
-//                 }
-//             }
-//         }
-//         if (!any_changed) {
-//             continue;
-//         }
-
-//         // Check for stale data in case of silent failure
-//         int64_t now = esp_timer_get_time();
-//         mpu_stale = ((now - local.mpu_timestamp_us) > stale_mpu_time) ||
-//                     (local.mpu_timestamp_us == 0);
-//         for(uint8_t sensor = 0; sensor < TOF_COUNT; sensor++){
-//             tof_stale[sensor] = ((now - local.tof_timestamp_us[sensor]) > stale_tof_time) ||
-//                     (local.tof_timestamp_us[sensor] == 0);
-//         }
-
-//         // Make csv line
-//         // log_time_s,mpu_time,pitch,roll,tof[i]_time,tof[i]_mm...
-//         size_t offset = 0;
-//         offset += snprintf(csv_line + offset, sizeof(csv_line) - offset,
-//                             "%.3f,%.3f",
-//                             now / 1e6,
-//                             local.mpu_timestamp_us / 1e6);
-//         if (mpu_stale) {
-//             offset += snprintf(csv_line + offset, sizeof(csv_line) - offset, ",NaN,NaN");
-//         }
-//         else {
-//             offset += snprintf(csv_line + offset, sizeof(csv_line) - offset,
-//                 ",%.3f,%.3f", local.pitch, local.roll);
-//         }
-//         // Append with howevermany tofs there are
-//         for (uint8_t sensor = 0; sensor < TOF_COUNT && offset < sizeof(csv_line); sensor++){
-//            offset += snprintf(csv_line + offset, sizeof(csv_line) - offset,
-//                ",%.3f,%d",
-//                local.tof_timestamp_us[sensor] / 1e6,
-//                tof_stale[sensor] ? -1 : local.ranges[sensor]);
-//         }
-//         offset += snprintf(csv_line + offset, sizeof(csv_line) - offset, "\n");
-
-//         // Send to ram buffer
-//         buffer_write(csv_line);
-
-//         // Put into telemetry queue
-//         if (telemetry_queue != NULL) {
-//             telemetry_msg_t tm;
-//             memset(&tm, 0, sizeof(tm));
-//             tm.len = (uint16_t)snprintf(tm.buf, TELEMETRY_MAX_LEN, "%s", csv_line);
-//             xQueueSend(telemetry_queue, &tm, 0);
-//         }
-//         last_snap = local;
-//     }
-// }
 
 void app_main()
 {
-    // init non volatile storage for wifi
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        nvs_flash_erase();
-        ret = nvs_flash_init();
-    }
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "nvs_flash_init failed: %d", ret);
-    }
-
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_ap();
-    wifi_init();
-
     // Setup Telemetry
     telemetry_init();
+    // Setup WiFi
+    wifi_init();
 
     //wait for button
     gpio_set_direction(START_BUTTON_GPIO, GPIO_MODE_INPUT);
@@ -465,11 +237,10 @@ void app_main()
 
     // create tasks
     telemetry_start_aggregator();
-    // xTaskCreate(&telemetry_aggregator, "tel_agg", 4096, NULL, 6, &telemetry_aggregator_handle);
     xTaskCreate(&blinky, "blinky", 2048, NULL, 5, NULL);
     xTaskCreate(&mpu_logging, "mpu", 4096, NULL, 5, NULL);
     xTaskCreate(&tof_logging, "tof", 4096, NULL, 5, NULL);
-    xTaskCreate(&telemetry_broadcast, "udp_bcast", 4096, NULL, 5, NULL);
+    wifi_start_udp_broadcast();
     // if (telemetry_queue != NULL) {
     // }
 }
