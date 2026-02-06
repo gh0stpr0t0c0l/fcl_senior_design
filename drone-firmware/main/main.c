9b8e831ac1f51718efe0674356467bd683e21887
GabeS <gabe@southwell.net>
2026-02-06 13:57:47 -0500

    Build for testing on hardware

--- drone-firmware/main/main.c
+++ drone-firmware/main/main.c
@@ -22,20 +22,20 @@
 
 // static const char *TAG = "Drone";
 
-void blinky(void *pvParameter)
-{
-    //gpio_pad_select_gpio(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
-        /* Blink off (output low) */
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
+// void blinky(void *pvParameter)
+// {
+//     //gpio_pad_select_gpio(BLINK_GPIO);
+//     /* Set the GPIO as a push/pull output */
+//     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+//     while(1) {
+//         /* Blink off (output low) */
+//         gpio_set_level(BLINK_GPIO, 0);
+//         vTaskDelay(1000 / portTICK_PERIOD_MS);
+//         /* Blink on (output high) */
+//         gpio_set_level(BLINK_GPIO, 1);
+//         vTaskDelay(1000 / portTICK_PERIOD_MS);
+//     }
+// }
 
 // void motor_test(void *pvParameter)
 // {
@@ -66,10 +66,10 @@ void app_main()
     buzzer_play(BUZZER_STARTUP);
 
     //wait for button
-    gpio_set_direction(START_BUTTON_GPIO, GPIO_MODE_INPUT);
-    while (gpio_get_level(START_BUTTON_GPIO) == 1) {
-        vTaskDelay(pdMS_TO_TICKS(50));
-    }
+    // gpio_set_direction(START_BUTTON_GPIO, GPIO_MODE_INPUT);
+    // while (gpio_get_level(START_BUTTON_GPIO) == 1) {
+    //     vTaskDelay(pdMS_TO_TICKS(50));
+    // }
     buzzer_play(BUZZER_START_BUTTON);
 
     esp_err_t ret = nvs_flash_init();
