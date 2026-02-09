2f9c9fea89ae6a65c31062116be6e4b93748ab8b
GabeS <gabe@southwell.net>
2026-02-09 09:54:54 -0500

    Wrote some test scripts

--- drone-firmware/components/motors/include/motors.h
+++ drone-firmware/components/motors/include/motors.h
@@ -59,17 +59,6 @@
 #define MOTOR2_GPIO  MOTOR_2_PIN
 #define MOTOR3_GPIO  MOTOR_3_PIN
 #define MOTOR4_GPIO  MOTOR_4_PIN
-// #ifdef CONFIG_TARGET_ESPLANE_V2_S2
-//     #define MOTOR3_GPIO  CONFIG_MOTOR01_PIN         // M1 for ESP32FC
-//     #define MOTOR4_GPIO  CONFIG_MOTOR02_PIN        // M2 for ESP32FC
-//     #define MOTOR1_GPIO  CONFIG_MOTOR03_PIN        // M3 for ESP32FC
-//     #define MOTOR2_GPIO  CONFIG_MOTOR04_PIN        // M4 for ESP32FC
-// #else
-//     #define MOTOR1_GPIO  CONFIG_MOTOR01_PIN         // M1 for ESP32FC
-//     #define MOTOR2_GPIO  CONFIG_MOTOR02_PIN        // M2 for ESP32FC
-//     #define MOTOR3_GPIO  CONFIG_MOTOR03_PIN        // M3 for ESP32FC
-//     #define MOTOR4_GPIO  CONFIG_MOTOR04_PIN        // M4 for ESP32FC
-// #endif
 
 #define MOT_PWM_CH1  4      // Motor M1 pwmchannel
 #define MOT_PWM_CH2  5      // Motor M2 pwmchannel
