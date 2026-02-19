7a99618f1a62698cea2dd7e0f70ddd561d958d62
GabeS <gabe@southwell.net>
2026-02-19 14:29:59 -0500

    Improved test control script

--- desktop_app/crazyflie/test.py
+++ desktop_app/crazyflie/test.py
@@ -10,19 +10,35 @@ from cflib.utils import uri_helper
 # Change if needed
 uri = uri_helper.uri_from_env(default="udp://192.168.43.42:2390")
 
-HOVER_THRUST = 3000  # Adjust as needed
+HOVER_THRUST = 30000  # Adjust as needed
 COMMAND_RATE_HZ = 50  # Setpoint update rate
 LOG_RATE_MS = 50  # 20 ms = 50 Hz logging
+MOTOR_LOG = 1
+JUST_LOG = 0
 
 
 def log_callback(timestamp, data, logconf):
-    print(
-        f"{timestamp} | "
-        f"roll: {data['stabilizer.roll']:.2f}, "
-        f"pitch: {data['stabilizer.pitch']:.2f}, "
-        # f"yaw: {data['stabalizer.yaw']:.2f}, "
-        f"thrust: {data['stabilizer.thrust']:.2f}"
-    )
+    if MOTOR_LOG:
+        print(
+            f"{timestamp} | "
+            f"MOTOR: "
+            f"{data['motor.m1']:5d} "
+            f"{data['motor.m2']:5d} "
+            f"{data['motor.m3']:5d} "
+            f"{data['motor.m4']:5d}   |   "
+            f"PWM: "
+            f"{data['pwm.m1_pwm']:5d} "
+            f"{data['pwm.m2_pwm']:5d} "
+            f"{data['pwm.m3_pwm']:5d} "
+            f"{data['pwm.m4_pwm']:5d}"
+        )
+    else:
+        print(
+            f"{timestamp} | "
+            f"roll: {data['stabilizer.roll']:.2f}, "
+            f"pitch: {data['stabilizer.pitch']:.2f}, "
+            f"thrust: {data['stabilizer.thrust']:.2f}"
+        )
 
 
 def run_spin_test(cf):
@@ -37,7 +53,10 @@ def run_spin_test(cf):
     try:
         while True:
             # zero roll, pitch, yaw-rate, constant thrust
-            cf.commander.send_setpoint(0, 0, 0, HOVER_THRUST)
+            if JUST_LOG:
+                cf.commander.send_setpoint(0, 0, 0, 0)
+            else:
+                cf.commander.send_setpoint(0, 0, 0, HOVER_THRUST)
             time.sleep(dt)
 
     except KeyboardInterrupt:
@@ -54,10 +73,20 @@ if __name__ == "__main__":
 
         # ---- Logging Setup ----
         log_config = LogConfig(name="Stab", period_in_ms=LOG_RATE_MS)
-        log_config.add_variable("stabilizer.roll", "float")
-        log_config.add_variable("stabilizer.pitch", "float")
-        # log_config.add_variable("stabalizer.yaw, float")
-        log_config.add_variable("stabilizer.thrust", "float")
+        if not MOTOR_LOG:
+            log_config.add_variable("stabilizer.roll", "float")
+            log_config.add_variable("stabilizer.pitch", "float")
+            # log_config.add_variable("stabalizer.yaw, float")
+            log_config.add_variable("stabilizer.thrust", "float")
+        else:
+            log_config.add_variable("motor.m1", "uint16_t")
+            log_config.add_variable("motor.m2", "uint16_t")
+            log_config.add_variable("motor.m3", "uint16_t")
+            log_config.add_variable("motor.m4", "uint16_t")
+            log_config.add_variable("pwm.m1_pwm", "uint16_t")
+            log_config.add_variable("pwm.m2_pwm", "uint16_t")
+            log_config.add_variable("pwm.m3_pwm", "uint16_t")
+            log_config.add_variable("pwm.m4_pwm", "uint16_t")
 
         cf.log.add_config(log_config)
 
