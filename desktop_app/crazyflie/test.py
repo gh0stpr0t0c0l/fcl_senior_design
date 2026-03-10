e13fced34db09788c9eda83fe0ac21eacc05fbc1
GabeS <gabe@southwell.net>
2026-03-10 15:28:52 -0400

    improved test.py with csv logging
    This reverts commit 56b9c89f9d6f7edbf2ff7b0352fbf471bee334d3.

--- desktop_app/crazyflie/test.py
+++ desktop_app/crazyflie/test.py
@@ -1,6 +1,5 @@
 import sys
 import time
-from calendar import JULY
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
@@ -20,43 +19,73 @@ FLIGHT_TYPE = 1  # 0=gimbal; 1=altitude
 
 
 def log_callback(timestamp, data, logconf):
-    if LOG_TYPE == 0:
-        print(
-            f"{timestamp} | "
-            f"MOTOR: "
-            f"{data['motor.m1']:5d} "
-            f"{data['motor.m2']:5d} "
-            f"{data['motor.m3']:5d} "
-            f"{data['motor.m4']:5d}   |   "
-            f"PWM: "
-            f"{data['pwm.m1_pwm']:5d} "
-            f"{data['pwm.m2_pwm']:5d} "
-            f"{data['pwm.m3_pwm']:5d} "
-            f"{data['pwm.m4_pwm']:5d}"
-        )
-    elif LOG_TYPE == 1:
-        print(
-            f"{timestamp} | "
-            # f"roll: {data['stabilizer.roll']:.2f}, "
-            # f"pitch: {data['stabilizer.pitch']:.2f}, "
-            # f"yaw: {data['stabilizer.yaw']:.2f}, "
-            f"ROLL: "
-            f"PRoll: {data['pid_rate.roll_outP']:.2f}, "
-            f"IRoll: {data['pid_rate.roll_outI']:.2f}, "
-            f"DRoll: {data['pid_rate.roll_outD']:.2f}, "
-            f"PPitch: {data['pid_rate.pitch_outP']:.2f}, "
-            f"IPitch: {data['pid_rate.pitch_outI']:.2f}, "
-            f"DPitch: {data['pid_rate.pitch_outD']:.2f}, "
-            # f"thrust: {data['stabilizer.thrust']:.2f}"
-        )
-    elif LOG_TYPE == 2:
-        print(
-            f"{timestamp} | "
-            f"State Estimate: "
-            f"X: {data['stateEstimate.x']:.2f}, "
-            f"Y: {data['stateEstimate.y']:.2f}, "
-            f"Z: {data['stateEstimate.z']:.2f}"
-        )
+    with open("flightDump.csv", "a") as log:
+        if LOG_TYPE == 0:
+            print(
+                f"{timestamp} | "
+                f"MOTOR: "
+                f"{data['motor.m1']:5d} "
+                f"{data['motor.m2']:5d} "
+                f"{data['motor.m3']:5d} "
+                f"{data['motor.m4']:5d}   |   "
+                f"PWM: "
+                f"{data['pwm.m1_pwm']:5d} "
+                f"{data['pwm.m2_pwm']:5d} "
+                f"{data['pwm.m3_pwm']:5d} "
+                f"{data['pwm.m4_pwm']:5d}"
+            )
+            log.write(
+                f"{data['motor.m1']:5d}, "
+                f"{data['motor.m2']:5d}, "
+                f"{data['motor.m3']:5d}, "
+                f"{data['motor.m4']:5d},  "
+                f"{data['pwm.m1_pwm']:5d}, "
+                f"{data['pwm.m2_pwm']:5d}, "
+                f"{data['pwm.m3_pwm']:5d}, "
+                f"{data['pwm.m4_pwm']:5d}, \n"
+            )
+
+        elif LOG_TYPE == 1:
+            print(
+                f"{timestamp} | "
+                # f"roll: {data['stabilizer.roll']:.2f}, "
+                # f"pitch: {data['stabilizer.pitch']:.2f}, "
+                # f"yaw: {data['stabilizer.yaw']:.2f}, "
+                f"ROLL: "
+                f"PRoll: {data['pid_rate.roll_outP']:.2f}, "
+                f"IRoll: {data['pid_rate.roll_outI']:.2f}, "
+                f"DRoll: {data['pid_rate.roll_outD']:.2f}, "
+                f"PPitch: {data['pid_rate.pitch_outP']:.2f}, "
+                f"IPitch: {data['pid_rate.pitch_outI']:.2f}, "
+                f"DPitch: {data['pid_rate.pitch_outD']:.2f}, "
+                # f"thrust: {data['stabilizer.thrust']:.2f}"
+            )
+            log.write(
+                # f"roll: {data['stabilizer.roll']:.2f}, "
+                # f"pitch: {data['stabilizer.pitch']:.2f}, "
+                # f"yaw: {data['stabilizer.yaw']:.2f}, "
+                f"PRoll: {data['pid_rate.roll_outP']:.2f}, "
+                f"IRoll: {data['pid_rate.roll_outI']:.2f}, "
+                f"DRoll: {data['pid_rate.roll_outD']:.2f}, "
+                f"PPitch: {data['pid_rate.pitch_outP']:.2f}, "
+                f"IPitch: {data['pid_rate.pitch_outI']:.2f}, "
+                f"DPitch: {data['pid_rate.pitch_outD']:.2f},\n "
+                # f"thrust: {data['stabilizer.thrust']:.2f}"
+            )
+
+        elif LOG_TYPE == 2:
+            print(
+                f"{timestamp} | "
+                f"State Estimate: "
+                f"X: {data['stateEstimate.x']:.2f}, "
+                f"Y: {data['stateEstimate.y']:.2f}, "
+                f"Z: {data['stateEstimate.z']:.2f}"
+            )
+            log.write(
+                f"X: {data['stateEstimate.x']:.2f}, "
+                f"Y: {data['stateEstimate.y']:.2f}, "
+                f"Z: {data['stateEstimate.z']:.2f},\n"
+            )
 
 
 def flight_test(cf):
@@ -122,30 +151,40 @@ if __name__ == "__main__":
 
         # ---- Logging Setup ----
         log_config = LogConfig(name="Stab", period_in_ms=LOG_RATE_MS)
-        if LOG_TYPE == 1:
-            # log_config.add_variable("stabilizer.roll", "float")
-            # log_config.add_variable("stabilizer.pitch", "float")
-            # log_config.add_variable("stabilizer.yaw", "float")
-            log_config.add_variable("pid_rate.roll_outP", "float")
-            log_config.add_variable("pid_rate.roll_outI", "float")
-            log_config.add_variable("pid_rate.roll_outD", "float")
-            log_config.add_variable("pid_rate.pitch_outP", "float")
-            log_config.add_variable("pid_rate.pitch_outI", "float")
-            log_config.add_variable("pid_rate.pitch_outD", "float")
-            # log_config.add_variable("stabilizer.thrust", "float")
-        elif LOG_TYPE == 0:
-            log_config.add_variable("motor.m1", "uint16_t")
-            log_config.add_variable("motor.m2", "uint16_t")
-            log_config.add_variable("motor.m3", "uint16_t")
-            log_config.add_variable("motor.m4", "uint16_t")
-            log_config.add_variable("pwm.m1_pwm", "uint16_t")
-            log_config.add_variable("pwm.m2_pwm", "uint16_t")
-            log_config.add_variable("pwm.m3_pwm", "uint16_t")
-            log_config.add_variable("pwm.m4_pwm", "uint16_t")
-        elif LOG_TYPE == 2:
-            log_config.add_variable("stateEstimate.x", "float")
-            log_config.add_variable("stateEstimate.y", "float")
-            log_config.add_variable("stateEstimate.z", "float")
+
+        with open("flightDump.csv", "w") as f:
+            if LOG_TYPE == 1:
+                # log_config.add_variable("stabilizer.roll", "float")
+                # log_config.add_variable("stabilizer.pitch", "float")
+                # log_config.add_variable("stabilizer.yaw", "float")
+                log_config.add_variable("pid_rate.roll_outP", "float")
+                log_config.add_variable("pid_rate.roll_outI", "float")
+                log_config.add_variable("pid_rate.roll_outD", "float")
+                log_config.add_variable("pid_rate.pitch_outP", "float")
+                log_config.add_variable("pid_rate.pitch_outI", "float")
+                log_config.add_variable("pid_rate.pitch_outD", "float")
+
+                f.write(
+                    "Roll P rate,Roll I rate,Roll D rate,Pitch P rate,Pitch I rate, Pitch D rate,\n"
+                )
+                # log_config.add_variable("stabilizer.thrust", "float")
+            elif LOG_TYPE == 0:
+                log_config.add_variable("motor.m1", "uint16_t")
+                log_config.add_variable("motor.m2", "uint16_t")
+                log_config.add_variable("motor.m3", "uint16_t")
+                log_config.add_variable("motor.m4", "uint16_t")
+                log_config.add_variable("pwm.m1_pwm", "uint16_t")
+                log_config.add_variable("pwm.m2_pwm", "uint16_t")
+                log_config.add_variable("pwm.m3_pwm", "uint16_t")
+                log_config.add_variable("pwm.m4_pwm", "uint16_t")
+
+                f.write("m1,m2,m3,m4,pwm1,pwm2,pwm3,pwm4,\n")
+            elif LOG_TYPE == 2:
+                log_config.add_variable("stateEstimate.x", "float")
+                log_config.add_variable("stateEstimate.y", "float")
+                log_config.add_variable("stateEstimate.z", "float")
+
+                f.write("x estimate,y estimate,z estimate,\n")
 
         cf.log.add_config(log_config)
 
