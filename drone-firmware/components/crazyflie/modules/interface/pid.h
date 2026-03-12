5c497f558ed3c6446760c25964f1f3cc9f0518b9
Blobs24 <bradyjoe@icloud.com>
2026-03-12 16:53:18 -0400

    Some tuning and other changes

--- drone-firmware/components/crazyflie/modules/interface/pid.h
+++ drone-firmware/components/crazyflie/modules/interface/pid.h
@@ -65,34 +65,34 @@
   #define DEFAULT_PID_INTEGRATION_LIMIT 5000.0
   #define DEFAULT_PID_OUTPUT_LIMIT      0.0
 #else
-  #define PID_ROLL_RATE_KP  250.0
-  #define PID_ROLL_RATE_KI  500.0
-  #define PID_ROLL_RATE_KD  2.5
+  #define PID_ROLL_RATE_KP  0.0//250.0
+  #define PID_ROLL_RATE_KI  0.0//500.0
+  #define PID_ROLL_RATE_KD  0.0//2.5
   #define PID_ROLL_RATE_INTEGRATION_LIMIT    33.3
 
-  #define PID_PITCH_RATE_KP  250.0
-  #define PID_PITCH_RATE_KI  500.0
-  #define PID_PITCH_RATE_KD  2.5
+  #define PID_PITCH_RATE_KP  80.0
+  #define PID_PITCH_RATE_KI  0.0//500.0
+  #define PID_PITCH_RATE_KD  0.0//2.5
   #define PID_PITCH_RATE_INTEGRATION_LIMIT   33.3
 
-  #define PID_YAW_RATE_KP  120.0
-  #define PID_YAW_RATE_KI  16.7
+  #define PID_YAW_RATE_KP  0.0//120.0
+  #define PID_YAW_RATE_KI  0.0//16.7
   #define PID_YAW_RATE_KD  0.0
   #define PID_YAW_RATE_INTEGRATION_LIMIT     166.7
 
-  #define PID_ROLL_KP  5.9
-  #define PID_ROLL_KI  2.9
-  #define PID_ROLL_KD  0.0
+  #define PID_ROLL_KP  0.0//5.9
+  #define PID_ROLL_KI  0.0//2.9
+  #define PID_ROLL_KD  0.0//0.0
   #define PID_ROLL_INTEGRATION_LIMIT    20.0
 
   #define PID_PITCH_KP  5.9
-  #define PID_PITCH_KI  2.9
+  #define PID_PITCH_KI  0.0//2.9
   #define PID_PITCH_KD  0.0
   #define PID_PITCH_INTEGRATION_LIMIT   20.0
 
-  #define PID_YAW_KP  6.0
-  #define PID_YAW_KI  1.0
-  #define PID_YAW_KD  0.35
+  #define PID_YAW_KP  0.0//6.0
+  #define PID_YAW_KI  0.0//1.0
+  #define PID_YAW_KD  0.0//0.35
   #define PID_YAW_INTEGRATION_LIMIT     360.0
 
 
